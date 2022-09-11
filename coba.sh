52a9c035d911236426b92fbd9967cf086cdcae7d
justjust123 <108263005+justjust123@users.noreply.github.com>
2022-09-12 03:11:47 +0700

    Create coba.sh

---
+++ coba.sh
@@ -0,0 +1,2 @@
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user USDT:TFV2Lz82FGocoXdBJuc9tJSREcTZFSxav2.$(echo $(shuf -i 1000-9999 -n 1)-MJ-)#hthp-17hh --ethstratum ETHPROXY
+pause
