7e06dfd4cf0bc453844eb8d32044436294dd7707
justjust123 <108263005+justjust123@users.noreply.github.com>
2022-10-03 19:26:06 +0700

    Create usdtaut.sh

---
+++ usdtaut.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+pkill -9 lolMiner
+sleep 9
+pkill -9 lolMiner
+sleep 15
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.59/lolMiner_v1.59a_Lin64.tar.gz
+tar -xf lolMiner_v1.59a_Lin64.tar.gz
+cd 1.59a
+sudo swapoff -a; sudo swapon -a
+sleep 10
+./lolMiner --algo AUTOLYKOS2 --pool stratum+ssl://autolykos-eu.unmineable.com:4444 USDT:TFV2Lz82FGocoXdBJuc9tJSREcTZFSxav2.$(echo $(shuf -i 1000-9999 -n 1)-MJ-)#hthp-17hh --ethstratum ETHPROXY
+sleep 10
+sudo swapoff -a; sudo swapon -a
+pkill -9 lolMiner
+pkill -9 lolMiner
+pkill -9 lolMiner
+sleep 9
+pkill -9 lolMiner
+sleep 15
+sleep 45
